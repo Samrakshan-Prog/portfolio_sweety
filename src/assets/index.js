de2ad7f653a6a2fc49e0bbaf7abb9f84cbdd7951
Srisamrakshan Parthiban <rakshan864@gmail.com>
2025-08-24 09:10:20 +0530

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,77 @@
+import bwmap from './backgrounds/bw-map.jpeg';
+import nairobi from './backgrounds/nairobi.png';
+import whiteabstract from './backgrounds/white-abstract.png';
+import worldmap from './backgrounds/world-map.png';
+
+import logo from './logo/logo-black.png';
+import logotext from './logo/logo-text-black.png';
+
+import sweety from './personal/sweety.png';
+
+import backend from './icons/backend.png';
+import ux from './icons/ux.png';
+import frontend from './icons/frontend.png';
+import prototyping from './icons/prototyping.png';
+import github from './icons/github.png';
+import close from './icons/close.png';
+import download from './icons/download.png';
+import downloadHover from './icons/downloadHover.png';
+import pineapple from './icons/pineapple.png';
+import pineappleHover from './icons/pineappleHover.png';
+import menu from './icons/menu.png';
+import resume from './icons/resume.png';
+import send from './icons/send.png';
+import sendHover from './icons/sendHover.png';
+
+import css from './tech/css.png';
+import git from './tech/git.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import nodejs from './tech/nodejs.png';
+import csharp from './tech/csharp.png';
+import java from './tech/java.png';
+import cpp from './tech/cpp.png';
+import php from './tech/php.png';
+import python from './tech/python.png';
+
+import dcc from './company/dcc.png';
+
+
+import f2h from './projects/f2h.jpg'
+
+export {
+  bwmap,
+  nairobi,
+  whiteabstract,
+  worldmap,
+  logo,
+  logotext,
+  sweety,
+  backend,
+  ux,
+  frontend,
+  prototyping,
+  github,
+  close,
+  download,
+  downloadHover,
+  pineapple,
+  pineappleHover,
+  menu,
+  resume,
+  send,
+  sendHover,
+  css,
+  git,
+  html,
+  javascript,
+  nodejs,
+  python,
+  php,
+  java,
+  cpp,
+  
+  f2h,
+  dcc,
+  csharp,
+};
